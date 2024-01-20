ea6703a1317fbe4f010c10da50547752890a713f
yuzeng <yuzeng@umich.edu>
2024-01-20 00:42:04 -0800

    make model deployable to gpu; add parser

--- mlm.py
+++ mlm.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.nn as nn
 import random
@@ -6,6 +7,12 @@ from dataloader import GenDataloader
 from config import batch_size
 import numpy as np
 import torch.optim as optim
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
+    parser.add_argument('--device', default='cpu',
+                        help='device to use for training / testing')
+    return parser
 
 # Define the Transformer Model
 class TransformerModel(nn.Module):
@@ -22,53 +29,65 @@ class TransformerModel(nn.Module):
         output = self.decoder(encoder_output)
         return output
 
+def main(args):
+  # check the visibility of the cuda
+  print('cuda is available: ', torch.cuda.is_available())
+  print('cuda device count: ', torch.cuda.device_count())
+  
+  # Hyperparameters
+  input_size = 16  # Size of each data in the sequence
+  num_heads = 4    # Number of heads in the multi-head attention models
+  num_layers = 3   # Number of sub-encoder-layers in the encoder
+  dim_feedforward = 512  # Dimension of the feedforward network model in nn.TransformerEncoder
+  max_seq_len = 8  # Maximum length of the input sequence
+  num_epochs = 1000
+  
+  # Initialize the model
+  model = TransformerModel(input_size, num_heads, num_layers, dim_feedforward, max_seq_len)
+  model = torch.nn.DataParallel(model)
+  model.to(args.device)
+  dataloader = GenDataloader("./synthetic_many_vars/data/1.csv")
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.Adam(model.parameters(), lr=0.001)  # Learning rate is 0.001 by default
+  
+  # Training Loop
+  for epoch in range(num_epochs):
+      model.train()  # Set the model to training mode
+      total_loss = 0
+  
+      for batch_idx, batch_data in enumerate(dataloader):
+          batch_data = batch_data[0]
+          # Masking a random element in each sequence of the batch
+          mask_indices = np.random.randint(max_seq_len-1, size=batch_size)
+          read_data = batch_data.clone()
+          masked_data = batch_data.clone()
+          for i, idx in enumerate(mask_indices):
+              # the dimensions: batch_size, seq_len(8), 
+              masked_data[i, idx, :] = 0
+  
+          # Forward pass
+          output = model(masked_data)
+          
+          # Calculate loss only for the masked elements
+          loss = sum(criterion(output[i, idx, :], read_data[i, idx, :]) for i, idx in enumerate(mask_indices)) / batch_size
+  
+          # Backpropagation
+          optimizer.zero_grad()
+          loss.backward()
+          optimizer.step()
+  
+          total_loss += loss.item()
+  
+      avg_loss = total_loss / len(dataloader)
+      print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}")
+  
+      # Add validation step here if you have a validation dataset
+      if epoch % 100 == 0:
+        # Save the trained model
+        torch.save(model.state_dict(), "transformer_model.pth")
 
-# Hyperparameters
-input_size = 16  # Size of each data in the sequence
-num_heads = 4    # Number of heads in the multi-head attention models
-num_layers = 3   # Number of sub-encoder-layers in the encoder
-dim_feedforward = 512  # Dimension of the feedforward network model in nn.TransformerEncoder
-max_seq_len = 8  # Maximum length of the input sequence
-num_epochs = 1000
 
-# Initialize the model
-model = TransformerModel(input_size, num_heads, num_layers, dim_feedforward, max_seq_len)
-dataloader = GenDataloader("./synthetic_many_vars/data/1.csv")
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)  # Learning rate is 0.001 by default
-
-# Training Loop
-for epoch in range(num_epochs):
-    model.train()  # Set the model to training mode
-    total_loss = 0
-
-    for batch_idx, batch_data in enumerate(dataloader):
-        batch_data = batch_data[0]
-        # Masking a random element in each sequence of the batch
-        mask_indices = np.random.randint(max_seq_len-1, size=batch_size)
-        read_data = batch_data.clone()
-        masked_data = batch_data.clone()
-        for i, idx in enumerate(mask_indices):
-            # the dimensions: batch_size, seq_len(8), 
-            masked_data[i, idx, :] = 0
-
-        # Forward pass
-        output = model(masked_data)
-        
-        # Calculate loss only for the masked elements
-        loss = sum(criterion(output[i, idx, :], read_data[i, idx, :]) for i, idx in enumerate(mask_indices)) / batch_size
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-
-    avg_loss = total_loss / len(dataloader)
-    print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}")
-
-    # Add validation step here if you have a validation dataset
-    if epoch % 100 == 0:
-      # Save the trained model
-      torch.save(model.state_dict(), "transformer_model.pth")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('masked language model for invariant generation', parents=[get_args_parser()])
+    args = parser.parse_args()
+    main(args)
